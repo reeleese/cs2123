07bf8ff87a8b78b739e48ef4c5f1fa46d7e5fee3
odq641 <odq641@fox01.cs.utsa.edu>
2018-08-28 12:16:31 -0500

    finished first couple of bullets

--- odq641-rec02/rec02.c
+++ odq641-rec02/rec02.c
@@ -6,6 +6,15 @@
 #include <stdio.h>
 
 int main() {
+  int temp, sum;
+  int a=10, b=20, c=30;
+  int *pa=&a, *pb=&b, *pc=&c;
+  int **ppa=&pa, **ppb=&pb, **ppc=&pc;
 
- return 0;
+  printf("a:%d b:%d c:%d\n", a, *pb, **ppc);
+
+  sum = a + *pb + **ppc;
+  printf("sum:%d\n", sum);
+  
+  return 0;
 }
