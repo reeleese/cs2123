616dff582426e107dfbd82b6e0c253b83b7b8fef
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-09-04 17:16:49 -0500

    nearly done, need to handle errors.

--- odq641-rec03/rec03.c
+++ odq641-rec03/rec03.c
@@ -1,16 +1,17 @@
-/*
-  Author: Lee Reese
-  Due: Monday 9/10
+/* Recitation 03
+ * Author: Lee Reese
+ * Due: Monday 9/10
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+/* Returns a random number in [low, high] */
 double RandomReal(double low, double high){
   double d;
- d = (double) rand() / ((double) RAND_MAX + 1);
- return (low + d * (high - low));
+  d = (double) rand() / ((double) RAND_MAX + 1);
+  return (low + d * (high - low));
 }
 
 int main() {
@@ -19,9 +20,9 @@ int main() {
   double minZ, maxZ;
   double **Arr2D;
 
-  /* iterators and accumulators*/
+  /* iterators and accumulators */
   int i, j;
-  double sum_total=0, sum_rows=0, sum_cols=0;
+  double sum_total=0, sum_row=0, sum_col=0;
 
   /* seed RNG */
   srand(time(NULL));
@@ -35,25 +36,44 @@ int main() {
   for (i = 0; i < X; i++)
     Arr2D[i] = (double *) malloc(Y * sizeof(double));
 
-  /* Assign each slot in array */
+  /* Assign each slot in array a random (or not) number */
   for (i = 0; i < X; i++) {
     for (j = 0; j < Y; j++) {
       if (minZ == maxZ)
         Arr2D[i][j] = minZ;
       else
         Arr2D[i][j] = RandomReal(minZ, maxZ);
-
-      printf("%5.2lf ", Arr2D[i][j]);
     }
-    printf("\n\n");
   }
 
+  /* Calculate and return overall sum of all entries */
   for (i = 0; i < X; i++)
     for(j = 0; j < Y; j++)
       sum_total += Arr2D[i][j];
   printf("Overall sum = %.1lf\n", sum_total);
+
+  /* Calculate and return sum of each row */
+  printf("Sum of each row\n");
+  for(i = 0; i < X; i++) {
+    for(j = 0; j < Y; j++) {
+      sum_row += Arr2D[i][j];
+    }
+    printf("Row%d = %.1lf\n", i, sum_row);
+    sum_row = 0;
+  }
+
+  /* Calculate and return sum of each column */
+  printf("Sum of each column\n");
+  for(j = 0; j < Y; j++) {
+    for(i = 0; i < X; i++) {
+      sum_col += Arr2D[i][j];
+    }
+    printf("%.1lf ", sum_col);
+    sum_col = 0;
+  }
+  printf("\n");
           
-  /* Free Array */
+  /* Free allocated memoryy */
   for (i = 0; i < X; i++)
     free(Arr2D[i]);
   free(Arr2D);
