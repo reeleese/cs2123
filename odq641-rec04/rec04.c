71c487835f30e07bbcbb99e54a024c89d768e980
odq641 <odq641@fox01.cs.utsa.edu>
2018-09-12 16:35:15 -0500

    added function to build rectangles

--- odq641-rec04/rec04.c
+++ odq641-rec04/rec04.c
@@ -1,6 +1,9 @@
 /*    Author: Lee Reese
  *    Assignment: Recitation 4
  */
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct Point {
   double x;
   double y;
@@ -13,7 +16,26 @@ typedef struct Rect {
   double height;
 } RectT;
 
-int main() {
+void buildRect(RectT *, char *);
 
+int main() {
+  RectT a, b;
+  RectT *recs;
+  
+  buildRect(&a, "a");
+  buildRect(&b, "b");
+
+// printf("%lf %lf %lf %lf", a.location.x, a.location.y, a.height, a.width);
   return 0;
+}
+
+/* #TODO: make this easier for the user */
+void buildRect(RectT *pRect, char *name) {
+  /* Fill location */
+  printf("Enter coordinates for %s.\n> ", name);
+  scanf("%lf %lf", &pRect->location.x, &pRect->location.y);
+
+  /* Fill height and width */
+  printf("Enter height and width for %s.\n> ", name);
+  scanf("%lf %lf", &pRect->height, &pRect->width);
 }
