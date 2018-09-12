9be7ec7f0590f7ebb8940306b408e0459aa1db81
odq641 <odq641@fox01.cs.utsa.edu>
2018-09-12 16:19:28 -0500

    initial commit

---
+++ odq641-rec04/rec04.c
@@ -0,0 +1,19 @@
+/*    Author: Lee Reese
+ *    Assignment: Recitation 4
+ */
+typedef struct Point {
+  double x;
+  double y;
+} PointT;
+
+typedef struct Rect {
+  PointT location;
+  char color;
+  double width;
+  double height;
+} RectT;
+
+int main() {
+
+  return 0;
+}
