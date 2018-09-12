2ca7ee83a33e22cdbcff8871cc467e5753d83157
odq641 <odq641@fox01.cs.utsa.edu>
2018-09-12 17:23:26 -0500

    added chk_overlap and subroutine point_in_rect

--- odq641-rec04/rec04.c
+++ odq641-rec04/rec04.c
@@ -17,6 +17,8 @@ typedef struct Rect {
 } RectT;
 
 void buildRect(RectT *, char *);
+int chk_overlap(RectT *, RectT *);
+int point_in_rect(RectT, PointT);
 
 int main() {
   RectT a, b;
@@ -25,7 +27,9 @@ int main() {
   buildRect(&a, "a");
   buildRect(&b, "b");
 
-// printf("%lf %lf %lf %lf", a.location.x, a.location.y, a.height, a.width);
+  // printf("%lf %lf %lf %lf", a.location.x, a.location.y, a.height, a.width);
+printf( (chk_overlap(&a, &b)? "yes\n":"no\n" ) ); 
+
   return 0;
 }
 
@@ -38,4 +42,54 @@ void buildRect(RectT *pRect, char *name) {
   /* Fill height and width */
   printf("Enter height and width for %s.\n> ", name);
   scanf("%lf %lf", &pRect->height, &pRect->width);
+}
+
+int point_in_rect(RectT r, PointT p) {
+  /* Establish bottom left and top right points on r */
+  PointT bottom_left = r.location;
+  PointT top_right;
+  top_right.x = bottom_left.x + r.width;
+  top_right.y = bottom_left.y + r.height;
+
+  /* Determine if p is within the vertical lines of r */
+  int bounded_in_x = 0;
+  if (bottom_left.x < p.x && p.x < top_right.x)
+    bounded_in_x = 1;
+ 
+  /* Determine if p is within the horizontal lines of r */
+  int bounded_in_y = 0;
+  if (bottom_left.y < p.y && p.y < top_right.y)
+    bounded_in_y = 1;
+
+  return bounded_in_x && bounded_in_y;  
+}
+
+/* check any corners on r2 lie within r1, going clockwise from
+ * bottom left
+ */
+int chk_overlap(RectT *r1, RectT *r2) {
+  PointT corner;
+
+  /* Bottom left */
+  corner.x = r2->location.x;
+  corner.y = r2->location.y;
+  if (point_in_rect(*r1, corner))
+    return 1;
+  
+  /* Top left */
+  corner.y += r2->height;
+  if (point_in_rect(*r1, corner))
+    return 1;
+
+  /* Top right */
+  corner.x += r2->width;
+  if (point_in_rect(*r1, corner))
+    return 1;
+
+  /* Bottom right */
+  corner.y -= r2->height;
+  if (point_in_rect(*r1, corner))
+    return 1;
+
+  return 0;
 }
