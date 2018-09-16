cff9315131834fa43b8924531ee0f4ab1d811de0
reeleese <reeleese@github.com>
2018-09-15 20:55:33 -0500

    added buildRandomRec() and randomReal(). Used them in main.

--- odq641-rec04/rec04.c
+++ odq641-rec04/rec04.c
@@ -3,6 +3,8 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
 typedef struct Point {
   double x;
@@ -16,25 +18,56 @@ typedef struct Rect {
   double height;
 } RectT;
 
-void buildRect(RectT *, char *);
+void buildUserRect(RectT *, char *);
+void buildRandomRect(RectT *);
 int chk_overlap(RectT *, RectT *);
+double randomReal(double low, double high);
 
 int main() {
+  /* Seed RNG */
+  srand( time(NULL) );
+
+  
   RectT a, b;
   RectT *recs;
-  while (1) {
   
-    buildRect(&a, "a");
-    buildRect(&b, "b");
+  /* Build user's rectangles */  
+  buildUserRect(&a, "a");
+  buildUserRect(&b, "b");
 
-    printf( (chk_overlap(&a, &b)? "not overlapping\n":"overlapping\n" ) );
+  /* Allocate recs */
+  recs = (RectT *) malloc(50*sizeof(RectT));
+  if (recs == NULL) {
+    printf("Failed to allocate recs. Exiting...\n");
+    return -1;
   }
 
+  /* Initialize recs */
+  int i;
+  for(i=0; i<50; i++)
+    buildRandomRect(&recs[i]);
+
+  /* For each RectT in recs, check for overlapping with user RectTs */
+  int over_a, over_b, over_both;
+  for(i=0; i<50; i++) {
+    /* establish where recs[i] is overlapping */
+    over_a = chk_overlap(&recs[i], &a);
+    over_b = chk_overlap(&recs[i], &b);
+    over_both = over_a && over_b;
+
+    /* print results */
+    
+  }
+    
+  
+
+  printf( (chk_overlap(&a, &b)? "overlapping\n":"not overlapping\n" ) );
+  
   return 0;
 }
 
 /* #TODO: make this easier for the user */
-void buildRect(RectT *pRect, char *name) {
+void buildUserRect(RectT *pRect, char *name) {
   /* Fill location */
   printf("Enter coordinates for %s.\n> ", name);
   scanf("%lf %lf", &pRect->location.x, &pRect->location.y);
@@ -42,6 +75,19 @@ void buildRect(RectT *pRect, char *name) {
   /* Fill height and width */
   printf("Enter height and width for %s.\n> ", name);
   scanf("%lf %lf", &pRect->height, &pRect->width);
+}
+
+void buildRandomRect(RectT *pRect) {
+  /* Bounds for random attributs of pRect */
+  int point_low = -100;
+  int point_high = 100;
+  int dim_low = 0;
+  int dim_high = 30;
+
+  pRect->location.x = randomReal(point_low, point_high);
+  pRect->location.y = randomReal(point_low, point_high);
+  pRect->height = randomReal(dim_low, dim_high);
+  pRect->width = randomReal(dim_low, dim_high);
 }
 
 /* check any corners on r2 lie within r1, going clockwise from
@@ -62,11 +108,17 @@ int chk_overlap(RectT *r1, RectT *r2) {
 
   /* Check if r1 is to the right of r2 OR r1 is to the left of r2 */
   if (r1_bl.x > r2_tr.x || r1_tr.x < r2_bl.x)
-    return 1;
+    return 0;
 
   /* Check if r1 is above r2 OR r1 is below r2 */
   if (r1_bl.y > r2_tr.y || r1_tr.y < r2_bl.y)
-    return 1;
+    return 0;
 
-  return 0;
+  return 1;
+}
+
+double randomReal(double low, double high){
+  double d;
+  d = (double) rand() / ((double) RAND_MAX + 1);
+  return (low + d * (high - low));
 }
