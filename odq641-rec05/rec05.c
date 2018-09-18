20baeab21ff358bbb4a3cf7577bbd414afbab50d
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-09-18 14:15:21 -0500

    refactored open file procedure into openCritPathInfo function

--- odq641-rec05/rec05.c
+++ odq641-rec05/rec05.c
@@ -6,44 +6,37 @@
 #include <stdio.h>
 #include <string.h>
 
-void printFileHeader(FILE *);
-void printFileFooter(FILE *, int);
+int openCritPathInfo(FILE **, FILE **, char *argv[]);
+void printHeader(FILE *);
+void printFooter(FILE *, int);
+void printEvent(FILE *, int, int, int);
 
 int main(int argc, char *argv[]) {
   FILE *infp=NULL, *outfp=NULL;
   int prevEvent, currEvent, numTasks;
   int days, maxDays, totalDays=0;
 
-  /* Currently only accupting -input then -output.
-   * #TODO: make this more general */
-  if (!strcmp(argv[1], "-input") && !strcmp(argv[3], "-output")) {
-    infp = fopen(argv[2], "r");
-    if (infp == NULL) {
-      printf("File could not be open\n");
-      return -1;
-    }
+  if (argc != 5)
+    printf("Incorrect number of arguments.\n");
 
-    outfp = fopen(argv[4], "w");
-    if (outfp == NULL) {
-      printf("File could not be open\n");
-      return -1;
-    }
-  }/* end if */
-
+  if (!openCritPathInfo(&infp, &outfp, argv)) {
+    printf("Failed to open file(s). Exiting...\n");
+    return -1;
+  }
+  
   /* Prime loop */
-  printFileHeader(outfp);
+  printHeader(outfp);
   fscanf(infp, "%d %*d %d", &prevEvent, &days);
   numTasks = 1;
  
   /* Loop */
   while(fscanf(infp, "%d %*d %d", &currEvent, &days) == 2) {
     if (prevEvent != currEvent) {
-      fprintf(outfp, "%-7d %-14d %-d\n", prevEvent, numTasks, maxDays);
+      printEvent(outfp, prevEvent, numTasks, maxDays);
       totalDays += maxDays;
       numTasks = 0;
       maxDays = 0;
     }
-    printf("made it\n");
     prevEvent = currEvent;
     numTasks++;
     if (days > maxDays)
@@ -51,9 +44,9 @@ int main(int argc, char *argv[]) {
   }
  
   /* Offload loop */
-  fprintf(outfp, "%-7d %-14d %-d\n", currEvent, numTasks, maxDays);
+  printEvent(outfp, currEvent, numTasks, maxDays);
   totalDays += maxDays;
-  printFileFooter(outfp, totalDays);
+  printFooter(outfp, totalDays);
 
   fclose(infp);
   fclose(outfp);
@@ -61,14 +54,38 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-void printFileHeader(FILE *out) {
+int openCritPathInfo(FILE **in, FILE **out, char *argv[]) {
+  /* Currently only accupting -input then -output.
+   * #TODO: make this more general */
+  if (!strcmp(argv[1], "-input") && !strcmp(argv[3], "-output")) {
+    *in = fopen(argv[2], "r");
+    if (*in == NULL) {
+      printf("File could not be open\n");
+      return 0;
+    }
+
+    *out = fopen(argv[4], "w");
+    if (*out == NULL) {
+      printf("File could not be open\n");
+      return 0;
+    }
+  }
+  
+  return 1;
+}
+
+void printHeader(FILE *out) {
   fprintf(out, "Project Completion Timetable\n");
   fprintf(out, "------------------------------------------------\n");
   fprintf(out, "Event   Num of Tasks   Max Num of Days\n");
   fprintf(out, "------  ------         ------\n");
 }
 
-void printFileFooter(FILE *out, int total) {
+void printFooter(FILE *out, int total) {
   fprintf(out, "------------------------------------------------\n");
   fprintf(out, "Total number of days to finish the project: %d days\n", total);
+}
+
+void printEvent(FILE *out, int eventNum, int numTasks, int maxDays) {
+  fprintf(out, "%-7d %-14d %-d\n", eventNum, numTasks, maxDays);
 }
