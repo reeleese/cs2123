382bd7e5ef114ba5a70305582ef809b036228771
Lee Reese <leereese@Lees-MacBook-Pro.local>
2018-11-14 12:40:51 -0600

    Idk what I did, it's been a minute

--- odq641-hw05/driver.c
+++ odq641-hw05/driver.c
@@ -4,6 +4,7 @@
 
 int equal(char *a, char *b);
 void listCommands();
+void print(int x);
 
 int main()
 {
@@ -16,61 +17,70 @@ int main()
 
     done = 0;
     while (!done) {
-	printf("Enter a command:\n >");
-	scanf("%s", command);
-	printf("%s\n", command);
-	
-	if (equal(command, "insert")) {
-	    scanf("%d", &arg);
-	    TreeInsert(myTree, arg);
-	} else if (equal(command, "find")) {
-	    scanf("%d", &arg);
-	    TreeFind(myTree, arg)? printf("Found.\n") : printf("Not Found.\n");
-	} else if (equal(command, "delete")) {
-	    scanf("%d", &arg);
-	    if ((target = TreeFind(myTree, arg)) != NULL)
-		TreeDelete(myTree, target);
-	    else
-		printf("%d is not in the tree.\n", arg);
-	} else if (equal(command, "inorder")) {
-	    TreePrintInorder(myTree);
-	} else if (equal(command, "preorder")) {
-	    TreePrintPreorder(myTree);
-	} else if (equal(command, "postorder")) {
-	    TreePrintPostorder(myTree);
-	} else if (equal(command, "levelorder")) {
-	    TreePrintLevelorder(myTree);
-	} else if (equal(command, "max")) {
-	    printf("MAX = %d\n", TreeMax(myTree));
-	} else if (equal(command, "min")) {
-	    printf("MIN = %d\n", TreeMin(myTree));
-	} else if (equal(command, "average")) {
-	    printf("AVERAGE = %lf\n", TreeAverage(myTree));
-	} else if (equal(command, "height")) {
-	    printf("HEIGHT = %d\n", TreeHeight(myTree));
-	} else if (equal(command, "count")) {
-	    printf("COUNT = %d\n", TreeCount(myTree));
-	} else if (equal(command, "sum")) {
-	    printf("SUM = %d\n", TreeSum(myTree));
-	} else if (equal(command, "quit")) {
-	    done = 1;
-	} else if (equal(command, "help")) {
-	    listCommands();
-	} else { /* Unrecognized command */
-	    printf("Unrecognized command. ");
-	    printf("Type \"help\" for a list of commands.\n");
-	}
+        printf("Enter a command:\n >");
+        scanf("%s", command);
+        
+        if (equal(command, "insert")) {
+            scanf("%d", &arg);
+            TreeInsert(&myTree, arg);
+            printf("\n");
+        } else if (equal(command, "find")) {
+            scanf("%d", &arg);
+            TreeFind(myTree, arg)? printf("Found.\n") : printf("Not Found.\n");
+        } else if (equal(command, "delete")) {
+            scanf("%d", &arg);
+            if ((target = TreeFind(myTree, arg)) != NULL)
+                TreeDelete(myTree, target);
+            else
+                printf("%d is not in the tree.\n", arg);
+        } else if (equal(command, "inorder")) {
+            TreeInorder(myTree, print);
+            printf("\n");
+        } else if (equal(command, "preorder")) {
+            TreePreorder(myTree, print);
+            printf("\n");
+        } else if (equal(command, "postorder")) {
+            TreePostorder(myTree, print);
+            printf("\n");
+        } else if (equal(command, "levelorder")) {
+            TreePrintLevelorder(myTree);
+        } else if (equal(command, "max")) {
+            printf("MAX = %d\n", TreeMax(myTree));
+        } else if (equal(command, "min")) {
+            printf("MIN = %d\n", TreeMin(myTree));
+        } else if (equal(command, "average")) {
+            printf("AVERAGE = %lf\n", TreeAverage(myTree));
+        } else if (equal(command, "height")) {
+            printf("HEIGHT = %d\n", TreeHeight(myTree));
+        } else if (equal(command, "count")) {
+            printf("COUNT = %d\n", TreeCount(myTree));
+        } else if (equal(command, "sum")) {
+            printf("SUM = %d\n", TreeSum(myTree));
+        } else if (equal(command, "quit")) {
+            TreeFree(myTree);
+            done = 1;
+        } else if (equal(command, "help")) {
+            listCommands();
+        } else { /* Unrecognized command */
+            printf("Unrecognized command. ");
+            printf("Type \"help\" for a list of commands.\n");
+        }
     }
 }
 
 int equal (char *a, char *b)
 {
     if (strcmp(a, b) == 0)
-	return 1;
+        return 1;
     return 0;
 }
 
 void listCommands()
 {
     printf("Commands:\n");
+}
+
+void print(int x)
+{
+    printf("%d ", x);
 }
